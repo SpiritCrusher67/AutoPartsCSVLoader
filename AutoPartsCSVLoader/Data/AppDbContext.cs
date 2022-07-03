1e65731a1542a0c492ffd341812ff4ce76841839
sanchez.101 <sanchez.101@mail.ru>
2022-07-03 11:44:59 +0300

    Добавлен DbContext

---
+++ AutoPartsCSVLoader/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AutoPartsCSVLoader.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<PriceItem> PriceItems => Set<PriceItem>();
+
+        public AppDbContext() => Database.EnsureCreated();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PriceItem>().Property(f => f.Vendor).HasMaxLength(64);
+            modelBuilder.Entity<PriceItem>().Property(f => f.Number).HasMaxLength(64);
+            modelBuilder.Entity<PriceItem>().Property(f => f.SearchVendor).HasMaxLength(64);
+            modelBuilder.Entity<PriceItem>().Property(f => f.SearchNumber).HasMaxLength(64);
+            modelBuilder.Entity<PriceItem>().Property(f => f.Description).HasMaxLength(512);
+            modelBuilder.Entity<PriceItem>().Property(f => f.Cost).HasPrecision(18, 2);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
