45fb63bfa4ee535ac43c086afbc23dbf1170cc92
sanchez.101 <sanchez.101@mail.ru>
2022-07-03 18:13:23 +0300

    Добавлен интерфейс IPriceItemDataDbLoader для загрузки данных из stream в БД

---
+++ AutoPartsCSVLoader/Interfaces/IPriceItemDataDbLoader.cs
@@ -0,0 +1,7 @@
+﻿namespace AutoPartsCSVLoader.Interfaces
+{
+    public interface IPriceItemDataDbLoader
+    {
+        Task LoadDataAsync(Stream dataStream, string suplierConfigurationName, CancellationToken cancellationToken = default);
+    }
+}
