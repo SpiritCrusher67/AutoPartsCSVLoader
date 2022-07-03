7c84e4e1a212f8d3c8c2b3e12fe8bbd2a246b03a
sanchez.101 <sanchez.101@mail.ru>
2022-07-03 11:48:52 +0300

    Контекст БД добавлен в коллекцию сервисов

--- AutoPartsCSVLoader/Program.cs
+++ AutoPartsCSVLoader/Program.cs
@@ -1,5 +1,19 @@
+using AutoPartsCSVLoader.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+RegisterServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
 app.Run();
+
+void RegisterServices(IServiceCollection services, IConfiguration configuration)
+{
+    var connectionString = configuration["DbConnection"];
+    services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
+
+}
