0cdcf2681040f097a8dd83fd09acb76ce05dedf5
sanchez.101 <sanchez.101@mail.ru>
2022-07-03 13:18:29 +0300

    Добавлен почтовый сервис для подключения и аутентификации на почтовом сервере

---
+++ AutoPartsCSVLoader/Services/MailService.cs
@@ -0,0 +1,44 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using System.Net;
+
+namespace AutoPartsCSVLoader.Services
+{
+    public class MailService
+    {
+        private readonly ImapClient _client;
+        private readonly IConfiguration _configuration;
+
+        public MailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _client = new();
+
+        }
+
+        public async Task ConnectAsync()
+        {
+            var host = _configuration["MailConfiguration:Host"];
+            var port = int.Parse(_configuration["MailConfiguration:Port"]);
+            var useSSL = bool.Parse(_configuration["MailConfiguration:UseSSL"]);
+
+            await _client.ConnectAsync(host, port, useSSL);
+        }
+
+        public async Task AuthenticateAsync()
+        {
+            var userName = _configuration["MailAuthenticationData:Email"];
+            var password = _configuration["MailAuthenticationData:Password"];
+            var credentials = new NetworkCredential(userName, password);
+
+            await _client.AuthenticateAsync(credentials);
+        }
+        public async Task<IMailFolder> GetInboxAsync()
+        {
+            var inbox = _client.Inbox;
+            await inbox.OpenAsync(FolderAccess.ReadOnly);
+
+            return inbox;
+        }
+    }
+}
