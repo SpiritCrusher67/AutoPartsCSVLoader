670f6d80c1fac6aea3255cfe7d81c3ac423afebe
sanchez.101 <sanchez.101@mail.ru>
2022-07-04 13:03:20 +0300

    Добавлен тестовый репозиторий

---
+++ AutoPartsCSVLoader.Tests/Common/TestPriceItemRepository.cs
@@ -0,0 +1,28 @@
+﻿using AutoPartsCSVLoader.Data;
+using AutoPartsCSVLoader.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AutoPartsCSVLoader.Tests.Common
+{
+    public class TestPriceItemRepository : IPriceItemRepository
+    {
+        public IList<PriceItem> PriceItems { get; private set; } = new List<PriceItem>();
+        public Task AddPriceItemAsync(PriceItem priceItem)
+        {
+            PriceItems.Add(priceItem);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public Task SaveAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+}
